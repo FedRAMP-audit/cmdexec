e7f8289aee8d7b38392120f2a61a37736f90e2c5
FedRAMP-audit <77427030+FedRAMP-audit@users.noreply.github.com>
2023-10-12 12:38:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import urllib.request
-page = 
-urllib.request.urlopen('http://von327fk7jq6f0ls3n5s824m4da4ywml.burp.schellman.info')
-print(page.read())
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
+url = 'http://von327fk7jq6f0ls3n5s824m4da4ywml.burp.schellman.info'
+response = urlopen(url)
+data = str(response.read())
